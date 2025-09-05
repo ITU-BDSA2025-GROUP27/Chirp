0043f3d0a375306580cd70497639273d3cc810bd
Lukas <luss@itu.dk>
2025-09-05 11:26:11 +0200

    Add class UserInterface

---
+++ Chirp.CLI/UserInterface.cs
@@ -0,0 +1,26 @@
+namespace Chirp.CLI;
+
+public static class UserInterface
+{
+    public static string timeFormat = "MM/dd/yy HH:mm:ss";
+
+    public static void PrintUsage()
+    {
+        Console.WriteLine("-------------");
+        Console.WriteLine("Usage:");
+        Console.WriteLine("dotnet run -- read | Prints all Cheeps");
+        Console.WriteLine("dotnet run -- cheep <message> | Cheep a message");
+        Console.WriteLine("-------------");
+    }
+
+    public static void PrintCheep(Cheep cheep)
+    {
+        Console.WriteLine($"{cheep.Author} @ {FromUnixTimeToDateTime(cheep.Timestamp)} : {cheep.Message}");
+    }
+
+    static string FromUnixTimeToDateTime(long timestamp)
+    {
+        var dto = DateTimeOffset.FromUnixTimeSeconds(timestamp).ToLocalTime();
+        return dto.ToString(timeFormat);
+    }
+}
