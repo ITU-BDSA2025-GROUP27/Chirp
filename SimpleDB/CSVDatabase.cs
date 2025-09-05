cbaf2cbe5440e9f0f790e7cfe318524d2500f59e
Lukas <luss@itu.dk>
2025-09-05 10:31:55 +0200

    Implement CSVDatabase and IDatabaseRepository for CSV file handling

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,30 @@
+using SimpleDB;
+using CsvHelper;
+using System.Globalization;
+
+sealed class CSVDatabase<T> : IDatabaseRepository<T>
+
+{
+    private readonly string _filePath;
+    public CSVDatabase(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        using StreamReader sr = new(_filePath);
+        using CsvReader cr = new(sr, CultureInfo.InvariantCulture);
+
+        return cr.GetRecords<T>().ToList();
+    }
+
+    public void Store(T record)
+    {
+        using StreamWriter sw = File.AppendText(_filePath);
+        using CsvWriter cw = new(sw, CultureInfo.InvariantCulture);
+
+        cw.WriteRecord(record);
+        cw.NextRecord();
+    }
+}
