88ebb7d28771c4649406c7ab2c11bf8cb812445a
Lukas <luss@itu.dk>
2025-10-05 12:42:47 +0200

    Add /cheep and /cheeps endpoints in CSVDBService

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -1,6 +1,25 @@
+using SimpleDB;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+string dbPath = "../../data/chirp_cli_db.csv";
+var db = CSVDatabase<Cheep>.Instance(dbPath);
+
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/cheeps", () =>
+{
+    var cheeps = db.Read();
+    return Results.Ok(cheeps);
+});
+
+app.MapPost("/cheep", (Cheep cheep) =>
+{
+    db.Store(cheep);
+    return Results.Ok();
+});
+
 app.Run();
+
+public record Cheep(string Author, string Message, long Timestamp);
