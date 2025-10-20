4be6ac86e7001a81d2180bb3e51b9230fbd68303
Lukas <luss@itu.dk>
2025-10-21 00:08:10 +0200

    Add database initialization and migration

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -15,6 +15,18 @@ builder.Services.AddScoped<ICheepService, CheepService>();
 
 var app = builder.Build();
 
+// Create a disposable service scope
+using (var scope = app.Services.CreateScope())
+{
+    // From the scope, get an instance of our database context.
+    // Through the `using` keyword, we make sure to dispose it after we are done.
+    using var context = scope.ServiceProvider.GetRequiredService<ChirpDBContext>();
+
+    // Execute the migration from code.
+    context.Database.Migrate();
+    DbInitializer.SeedDatabase(context);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
