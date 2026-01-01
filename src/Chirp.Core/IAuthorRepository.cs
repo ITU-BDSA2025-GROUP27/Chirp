533a5616acf1ec5d3f4e401f2b475865dd386c38
Lukas <luss@itu.dk>
2026-01-02 00:33:18 +0100

    Add in-code documentation to interface methods

--- src/Chirp.Core/IAuthorRepository.cs
+++ src/Chirp.Core/IAuthorRepository.cs
@@ -6,13 +6,41 @@ namespace Chirp.Core;
 public interface IAuthorRepository
 {
     // Queries
+
+    /// <summary>
+    /// Finds an author by their username.
+    /// </summary>
     Task<AuthorDTO?> FindAuthorByName(string name);
+
+    /// <summary>
+    /// Finds an author by their email address.
+    /// </summary>
     Task<AuthorDTO?> FindAuthorByEmail(string email);
+
+    /// <summary>
+    /// Checks if one author is following another.
+    /// </summary>
     Task<bool> IsFollowing(string followerName, string followedName);
+
+    /// <summary>
+    /// Gets the list of authors that a specific author is following.
+    /// </summary>
     Task<List<AuthorDTO>> GetFollowing(string authorName);
 
     // Commands
+
+    /// <summary>
+    /// Creates a new author.
+    /// </summary>
     Task CreateAuthor(Author author);
+
+    /// <summary>
+    /// Makes one author follow another.
+    /// </summary>
     Task FollowAuthor(string followerName, string followedName);
+
+    /// <summary>
+    /// Makes one author unfollow another.
+    /// </summary>
     Task UnfollowAuthor(string followerName, string followedName);
 }
--- src/Chirp.Core/IAuthorService.cs
+++ src/Chirp.Core/IAuthorService.cs
@@ -5,8 +5,23 @@ namespace Chirp.Core;
 /// </summary>
 public interface IAuthorService
 {
+    /// <summary>
+    /// Checks if one author is following another.
+    /// </summary>
     Task<bool> IsFollowing(string followerName, string followedName);
+
+    /// <summary>
+    /// Gets the list of authors that a specific author is following.
+    /// </summary>
     Task<List<AuthorDTO>> GetFollowing(string authorName);
+
+    /// <summary>
+    /// Makes one author follow another.
+    /// </summary>
     Task FollowAuthor(string followerName, string followedName);
+
+    /// <summary>
+    /// Makes one author unfollow another.
+    /// </summary>
     Task UnfollowAuthor(string followerName, string followedName);
 }
